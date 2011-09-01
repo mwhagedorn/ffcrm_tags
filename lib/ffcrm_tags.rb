7596470929822eba51e4e3bade01bf092c5004b0
Mike Hagedorn <mike@silverchairsolutions.com>
2011-09-01 12:48:13 -0500

    directory added

--- lib/ffcrm_tags.rb
+++ lib/ffcrm_tags.rb
@@ -1,3 +1,5 @@
+directory = File.expand_path(File.dirname(__FILE__))
+
 require File.join(directory, 'ffcrm_tags', 'models')
 require File.join(directory, 'ffcrm_tags', 'controller_actions')
 require File.join(directory, 'ffcrm_tags', 'view_helpers')
