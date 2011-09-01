2344e4bcfe3277d7e8453e4ca7265887e21e7428
Mike Hagedorn <mike@silverchairsolutions.com>
2011-09-01 12:29:05 -0500

    Require version

--- lib/ffcrm_tags.rb
+++ lib/ffcrm_tags.rb
@@ -3,5 +3,6 @@ require File.join(directory, 'ffcrm_tags', 'controller_actions')
 require File.join(directory, 'ffcrm_tags', 'view_helpers')
 require File.join(directory, 'ffcrm_tags', 'controller_hooks')
 require File.join(directory, 'ffcrm_tags', 'view_hooks')
+require File.join(directory, 'ffcrm_tags', 'version')
 
 
